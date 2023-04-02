1140f0e02ac84f7617ecd36203dd9cd5c675f9a2
Filipe Pina <fopina@gmail.com>
2023-04-03 00:04:18 +0100

    `--line-limit` to split input into smaller batches to close #2

--- Makefile
+++ Makefile
@@ -7,5 +7,8 @@ cleantest:
 test: cleantest
 	go run main.go --all --output testdata/output/ -- -v testdata/inp1.txt
 
+test2: cleantest
+	go run main.go --all -l 3 --output testdata/output/ -- -v testdata/inp1.txt
+
 dockertest: cleantest build
 	docker run --rm -v $(PWD)/testdata:/input tmp-$(notdir $(CURDIR)) /input/inp1.txt
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"log"
 	"os"
@@ -11,23 +12,11 @@ import (
 )
 
 type localOptions struct {
-	all bool // resolve for all record types
+	all       bool // resolve for all record types
+	lineLimit int  // split input to have (partial) results sooner
 }
 
-func main() {
-	s := scanner.Scanner{
-		Name: "dnsx",
-	}
-	moreOptions := &localOptions{}
-	options := s.BuildOptions()
-	flag.BoolVar(&moreOptions.all, "all", false, "Resolve for all DNS record types (same as using all the extra flags `-a -aaaa -cname ...` together)")
-	scanner.ParseOptions(options)
-
-	err := os.MkdirAll(options.Output, 0755)
-	if err != nil {
-		log.Fatalf("%v", err)
-	}
-
+func scan(s *scanner.Scanner, moreOptions *localOptions, input, output string) {
 	// pass temporary file to dnsx instead of final path, as only finished files should be placed there
 	file, err := os.CreateTemp("", "dnsx")
 	if err != nil {
@@ -40,22 +29,15 @@ func main() {
 		"-o", file.Name(),
 		"-duc",
 		"-l",
-		options.Input,
+		input,
 	}
 	if moreOptions.all {
 		flags = append(
 			flags,
-			"-a",
-			"-aaaa",
-			"-cname",
-			"-ns",
-			"-txt",
-			"-srv",
-			"-ptr",
-			"-mx",
-			"-soa",
-			"-axfr",
-			"-caa",
+			"-a", "-aaaa", "-cname",
+			"-ns", "-txt", "-srv",
+			"-ptr", "-mx", "-soa",
+			"-axfr", "-caa",
 		)
 	}
 	err = s.Exec(flags...)
@@ -63,7 +45,7 @@ func main() {
 		log.Fatalf("Failed to run scanner: %v", err)
 	}
 
-	realOutputFile := path.Join(options.Output, "output.txt")
+	realOutputFile := path.Join(s.Options.Output, output)
 	outputFile, err := os.Create(realOutputFile)
 	if err != nil {
 		log.Fatalf("Couldn't open dest file: %v", err)
@@ -72,5 +54,51 @@ func main() {
 	_, err = io.Copy(outputFile, file)
 	if err != nil {
 		log.Fatalf("Writing to output file failed: %v", err)
+	}
+}
+
+func main() {
+	s := scanner.Scanner{
+		Name: "dnsx",
+	}
+	moreOptions := &localOptions{}
+	options := s.BuildOptions()
+	flag.BoolVar(&moreOptions.all, "all", false, "Resolve for all DNS record types (same as using all the extra flags `-a -aaaa -cname ...` together)")
+	flag.IntVarP(&moreOptions.lineLimit, "line-limit", "l", 0, "Split input to have (partial) results sooner")
+	scanner.ParseOptions(options)
+
+	err := os.MkdirAll(options.Output, 0755)
+	if err != nil {
+		log.Fatalf("%v", err)
+	}
+
+	if moreOptions.lineLimit == 0 {
+		scan(&s, moreOptions, options.Input, "output.txt")
+	} else {
+		file, err := os.CreateTemp("", "dnsx")
+		if err != nil {
+			log.Fatalf("%v", err)
+		}
+		defer os.Remove(file.Name())
+		pending := 0
+		block := 0
+
+		scanner.ReadInputLines(options, func(hostname string) bool {
+			file.Write([]byte(hostname))
+			file.Write([]byte("\n"))
+			pending++
+			if pending >= moreOptions.lineLimit {
+				block++
+				scan(&s, moreOptions, file.Name(), fmt.Sprintf("output_%d.txt", block))
+				pending = 0
+				file.Truncate(0)
+				file.Seek(0, 0)
+			}
+			return true
+		})
+		if pending > 0 {
+			block++
+			scan(&s, moreOptions, file.Name(), fmt.Sprintf("output_%d.txt", block))
+		}
 	}
 }
